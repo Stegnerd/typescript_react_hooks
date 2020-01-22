f7d1d2816c3ee5a68b008479ff21e52855447fd3
dstegner <dmstegner@gmail.com>
2020-01-21 22:14:49 -0500

    init commit

---
+++ src/components/StateHooksComponent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const StateHooksComponent: React.FC<Props> = ({}) => {
+  return <div>State Hooks Component</div>;
+};
+
+export default StateHooksComponent;
